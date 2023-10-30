0ecae07075bc3e932190ef42f8e98b2790827d7c
jgrbomb <nrj.serg@gmail.com>
2023-10-30 17:33:45 -0400

    Loop example 0

---
+++ LoopsEx0.java
@@ -0,0 +1,9 @@
+public class LoopsEx0 {
+    public static void main(String[] args){
+        String greeting = "Hello World";
+        for(int i = 0; i < greeting.length();i++){
+            System.out.println(greeting.charAt(i));
+        }
+
+    }
+}
